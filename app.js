251b512e6519d8bc716b212616f73a09fb836756
injamul haque <ihsomrat.official@gmail.com>
2022-11-14 20:52:03 -0800

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+//! Open when someone clicks on the navigatin button 
+
+function openNav() {
+    document.getElementById("myNav").style.height = "85%";
+}
+
+
+//! Close when someone clicks on the Cross button 
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
